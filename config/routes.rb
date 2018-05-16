3ad72b70d7369df6cd9ffb09b9779fb82f9c03c5
Jack <jdcorley@gmail.com>
2018-05-16 09:40:21 -0400

    user sign up and sign in complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {sessions: 'users/sessions'}
+  devise_scope :user do 
+    get 'sign_in', to: 'devise/sessions#new'
+  end 
   get 'welcome/index'
   get 'welcome/about'
   root 'welcome#index'
