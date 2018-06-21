60d82498bf95cd1c96df995e57d22d4000558821
Jack <jdcorley@gmail.com>
2018-06-21 08:17:03 -0400

    mailgun in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'incoming/create'
   devise_for :users, controllers: {sessions: 'users/sessions'}
   devise_scope :user do 
     get 'sign_in', to: 'devise/sessions#new'
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
   resources :topics, shallow: true  do 
     resources :bookmarks
   end 
+
+  post :incoming, to: 'incoming#create'
 
   get 'about' => 'welcome#about'
   root 'welcome#index'
---
+++ spec/controllers/incoming_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe IncomingController, type: :controller do
+
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
+r
