22f4344935fe97dd4b7eec7a57a4243cd4c87620
Jack <jdcorley@gmail.com>
2018-06-22 17:03:22 -0400

    current_user set

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,7 +3,6 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, :authenticate_user!
 
   def create
-
     puts "INCOMING PARAMS HERE: #{params}"
 
     user_from_email    = params[:sender]
@@ -11,22 +10,23 @@ class IncomingController < ApplicationController
     bookmark_from_email = params['stripped-text']
     puts user_from_email, topic_from_email, bookmark_from_email 
 
-    if topic_nil(topic_from_email)
-      Topic.create!(title: topic_from_email)
-      topic_from_email = Topic.last
-      puts topic_from_email
-    end 
-
     if user_nil(user_from_email)
       User.create!(email: user_from_email)
       user_from_email = User.last
       puts user_from_email
     end
 
-    
-    puts user_from_email, topic_from_email, bookmark_from_email
     current_user = user_from_email
     puts current_user 
+
+    if topic_nil(topic_from_email)
+      current_user.topics.create!(title: topic_from_email)
+      topic_from_email = Topic.last
+      puts topic_from_email
+    end 
+
+    puts user_from_email, topic_from_email, bookmark_from_email
+    
     create_a_bookmark(user_from_email, topic_from_email, bookmark_from_email)
     
     head 200
