afc7a69c86905247a4ee6394691a67fb65cd2c05
Jack <jdcorley@gmail.com>
2018-06-22 14:16:52 -0400

    nope

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,6 +1,6 @@
 class IncomingController < ApplicationController
   # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
-  skip_before_action :verify_authenticity_token!
+  skip_before_action :verify_authenticity_token
 
   def create
      # Take a look at these in your server logs
