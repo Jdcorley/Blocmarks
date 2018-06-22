4ee731242c3597669fd9eb236f5b0d0dd059603f
Jack <jdcorley@gmail.com>
2018-06-22 15:59:25 -0400

    so close

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -7,20 +7,20 @@ class IncomingController < ApplicationController
      # to get a sense of what you're dealing with.
     puts "INCOMING PARAMS HERE: #{params}"
 
-    user_from_email    = params[:sender]
-    topic_from_email   = params[:subject]
-    bookmark_from_email = params['body-plain']
+    puts user_from_email    = params[:sender]
+    puts topic_from_email   = params[:subject]
+    puts bookmark_from_email = params['stripped-text']
     # body_without_quotes = request.POST.get('stripped-text', '')
     # recipient = request.POST.get('recipient')
     if user_nil(user_from_email)
-      user_from_email = User.create!(email: user_from_email)
+      puts user_from_email = User.create!(email: user_from_email)
     end
 
     if topic_nil(topic_from_email)
-      topic_from_email = Topic.create!(title: topic_from_email)
+     puts topic_from_email = Topic.create!(title: topic_from_email)
     end 
 
-    create_a_bookmark(user_from_email, topic_from_email, bookmark_from_email)
+   puts create_a_bookmark(user_from_email, topic_from_email, bookmark_from_email)
     
     head 200
   end
