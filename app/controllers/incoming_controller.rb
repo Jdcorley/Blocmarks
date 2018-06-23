508abc211bcef1f2f527a8faf2f3500dacea43ec
Jack <jdcorley@gmail.com>
2018-06-22 21:01:32 -0400

    and some final touches..

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -18,7 +18,7 @@ class IncomingController < ApplicationController
     puts current_user
 
     if topic_nil(topic_from_email)
-      topic_from_email = current_user.topics.create!(title: topic_from_email)
+      topic_from_email = current_user.Topic.create!(title: topic_from_email)
     end 
 
     puts topic_from_email
