a5b9f1b147baba8564de125a644d7a9a009eaee6
Jack <jdcorley@gmail.com>
2018-06-30 15:22:40 -0400

    incoming emails optimized

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,28 +4,22 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, :authenticate_user!
 
   def create
-    # strip incoming email for the goods
-    puts "INCOMING PARAMS HERE: #{params}"
     user_from_email    = params[:sender]
     topic_from_email   = params[:subject]
     bookmark_from_email = params['stripped-text']
-    puts user_from_email, topic_from_email, bookmark_from_email
-    # user_nil? if so create new user and send password reset
+
     if user_nil?(user_from_email)
       User.create!(email: user_from_email, 
                    password: SecureRandom.hex,
                    confirmed_at: Time.now).send_reset_password_instructions
     end
-    # set current_user to user from the email
     current_user = User.find_by_email(user_from_email)
-    puts current_user
-    # topic_nil? if so create new topic with topic parsed from email
+
     if topic_nil?(topic_from_email)
       current_user.topics.create!(title: topic_from_email)
-    end 
-    # set the topic to this_topic 
+    end
     this_topic = Topic.find_by(title: topic_from_email)
-    # create the bookmark with this_topic and the bookmark url from email
+
     this_topic.bookmarks.create!(url: bookmark_from_email, user: current_user )
     
     head 200
