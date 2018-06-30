c79807707b16627ef719dcc69cc973bcd57efa49
Jack <jdcorley@gmail.com>
2018-06-30 15:06:53 -0400

    w password reset improvment

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,6 +4,7 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, :authenticate_user!
 
   def create
+    new_user_created = false
     # strip incoming email for the goods
     puts "INCOMING PARAMS HERE: #{params}"
     user_from_email    = params[:sender]
@@ -12,9 +13,7 @@ class IncomingController < ApplicationController
     puts user_from_email, topic_from_email, bookmark_from_email
     # user_nil? if so create new user and send password reset
     if user_nil?(user_from_email)
-      User.create!(email: user_from_email, 
-                   password: SecureRandom.hex,
-                   confirmed_at: Time.now)
+      create_new_user(user_from_email, new_user_created)
     end
     # set current_user to user from the email
     current_user = User.find_by_email(user_from_email)
@@ -27,23 +26,28 @@ class IncomingController < ApplicationController
     this_topic = Topic.find_by(title: topic_from_email)
     # create the bookmark with this_topic and the bookmark url from email
     this_topic.bookmarks.create!(url: bookmark_from_email, user: current_user )
-    send_password_reset?(current_user)
+    send_password_reset?(current_user, new_user_created)
     
     head 200
   end
 
   def user_nil?(user_from_email)
-    User.find_by(email: user_from_email).nil?
+    User.find_by(email: user_from_email)
   end 
 
   def topic_nil?(topic_from_email)
     Topic.find_by(title: topic_from_email).nil?
   end 
 
-  def send_password_reset?(current_user)
-    puts "Current user is #{current_user}"
-    puts "#{User.last} is the last user."
-    if current_user == User.last
+  def create_new_user(user_from_email, new_user_created)
+    User.create!(email: user_from_email, 
+                 password: SecureRandom.hex,
+                 confirmed_at: Time.now)
+    new_user_created = true 
+  end 
+
+  def send_password_reset?(current_user, new_user_created)
+    if new_user_created 
       current_user.send_reset_password_instructions
     end 
   end
